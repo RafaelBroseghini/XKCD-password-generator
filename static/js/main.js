f4ebcf7c17058712ba89e3d74c2843bff9e0b044
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-05-03 21:49:21 -0500

    new clipboard plugin

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,19 @@
+var clipboard = new ClipboardJS('.btn2');
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
+
+
 function refresh() {
   location.reload();
 }
