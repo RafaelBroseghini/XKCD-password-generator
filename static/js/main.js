9cff3889148ee65c6bf5307d6f36b50e24a92de5
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-05-06 21:22:16 -0500

    popover on click

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,16 @@
 var clipboard = new ClipboardJS('.btn2');
 
+$(function () {
+  $('[data-toggle="popover"]').popover({
+    delay: {"show":500}
+  });
+})
+
 clipboard.on('success', function(e) {
+    console.log(clipboard);
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
-
     e.clearSelection();
 });
 
@@ -12,7 +18,6 @@ clipboard.on('error', function(e) {
     console.error('Action:', e.action);
     console.error('Trigger:', e.trigger);
 });
-
 
 function refresh() {
   location.reload();
